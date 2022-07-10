3fa5508a3d8e8bf16e7d3bb8cd75a7672d9b1284
Denys-Zabolotskyi <70952705+Denys-Zabolotskyi@users.noreply.github.com>
2022-07-10 21:43:53 +0300

    Сompleted task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const btnChangeColor = document.querySelector('.change-color');
+console.log(btnChangeColor);
+const bodyEl = document.querySelector('body');
+console.log(bodyEl);
+const spanEl = document.querySelector('span');
+console.log(spanEl);
+
+btnChangeColor.addEventListener('click', onBtnChangeColorClick);
+
+function onBtnChangeColorClick(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
