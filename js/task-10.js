658de45c5ab340d3aef364d4a1d244b9ffe3b199
Denys-Zabolotskyi <70952705+Denys-Zabolotskyi@users.noreply.github.com>
2022-07-10 23:59:55 +0300

    In process !!! task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,21 @@
+const inputEl = document.querySelector('input');
+console.log(inputEl);
+const btnCreate = document.querySelector('button[data-create]');
+console.log(btnCreate);
+const btnDestroy = document.querySelector('button[data-destroy]');
+console.log(btnDestroy);
+const divBoxesEl = document.querySelector('#boxes');
+console.log(divBoxesEl);
+
+inputEl.addEventListener('input', onInputСhange);
+function onInputСhange(event) {
+  console.log(event.currentTarget.value);
+}
+function createBoxes(amount) {
+  log('djghdjjk');
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+//***********Test* */
