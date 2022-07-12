a1ecfd13484aad0ceaae4275ba0cb71a39c69e57
Denys-Zabolotskyi <70952705+Denys-Zabolotskyi@users.noreply.github.com>
2022-07-12 13:47:56 +0300

    Сompleted task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
 
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  //   console.log(formElements);
+  const email = formElements.email.value;
+  //   console.log(`email: ${email}`);
+  const password = formElements.password.value;
+  //   console.log(`password: ${password}`);
+  if (password.length === 0 || email.length === 0) {
+    return alert('Please, fill all fields!');
+  }
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+}
+//****best for form data collection */
+// form.addEventListener('submit', onFormSubmit);
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const formData = new FormData(event.currentTarget);
+//   console.log(formData);
+// }
