68ddc4e0ba7902e7de2cad7a951d9a714a540a5b
Denys-Zabolotskyi <d.zabolotskyi@gmail.com>
2022-07-14 17:04:36 +0300

    Bug fix task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,47 +1,10 @@
-// const refs = {
-//   inputEl: document.querySelector('input'),
-//   btnCreate: document.querySelector('button[data-create]'),
-//   btnDestroy: document.querySelector('button[data-destroy]'),
-//   boxesEl: document.querySelector('#boxes'),
-// };
-
-// //************addEventListener******************** */
-// refs.btnCreate.addEventListener('click', createBoxes);
-// refs.btnDestroy.addEventListener('click', destroyBoxes);
-// //****************function******************************** */
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// function createBoxes(amount) {
-//   amount = Number(refs.inputEl.value);
-
-//   const boxesArray = [];
-
-//   for (let i = 0; i < amount; i += 1) {
-//     let size = 30 + 10 * i;
-//     boxesArray[i] = document.createElement('div');
-//     boxesArray[i].style.width = `${size}px`;
-//     boxesArray[i].style.height = `${size}px`;
-//     boxesArray[i].style.backgroundColor = getRandomHexColor();
-//   }
-
-//   refs.boxesEl.append(...boxesArray);
-// }
-
-// function destroyBoxes() {
-//   refs.boxesEl.innerHTML = '';
-// }
-
 const createBtn = document.querySelector('[data-create]');
 const destroyBtn = document.querySelector('[data-destroy]');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('#controls');
 const containerEl = document.querySelector('#boxes');
 
-createBtn.addEventListener('click', () => createBoxes(inputEl.value));
+createBtn.addEventListener('click', () => createBoxes(Number(inputEl.value)));
 destroyBtn.addEventListener('click', destroyBoxes);
 
 function createBoxes(amount) {
